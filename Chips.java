8b1a8913f8f0b768423ed703b22e092d7bf73b87
treadwaymax <77028403+treadwaymax@users.noreply.github.com>
2021-03-20 14:35:49 -0500

    Create Chips.java

---
+++ Chips.java
@@ -0,0 +1,18 @@
+package lab4;
+
+public class Chips extends Food{
+	static final int CHIPS_CALORIES = 200;
+
+	/**Chips() increments calories by CHIPS_CALORIES
+	 * It then prints the message "Serving Chips" */
+	
+	public Chips() {
+		calories += CHIPS_CALORIES;
+		System.out.println("Serving Chips");
+	}
+
+	/* eat() method returns the String "crunch crunch" */
+	public String eat() {
+		return "Crunch Crunch";
+	}
+}
