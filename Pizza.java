a76e571a19d5719d9ae23d6878a2a9b074d00a71
treadwaymax <77028403+treadwaymax@users.noreply.github.com>
2021-03-20 14:36:19 -0500

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,24 @@
+package lab4;
+
+public class Pizza extends Food implements Heatable {
+	static final int PIZZA_CALORIES = 800;
+
+	public Pizza() {
+		//increment total calories by 800
+		calories += PIZZA_CALORIES;
+		System.out.println("Serving Pizza");
+	}
+	
+	@Override
+	public void heatIt(){
+		// sets pizza food temp to 165
+		temperature = Heatable.HOTSERVINGTEMPERATURE;
+		System.out.println("Now its hot @ 165 degrees");
+	}
+
+	@Override
+	public String eat() {
+		// this string object is returned to eat() method in eatFood()
+		return "Chomp Chomp";
+	}
+}
