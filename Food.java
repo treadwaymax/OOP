cbc8e7f03ead59bfd48772f9141556f0451fee18
treadwaymax <77028403+treadwaymax@users.noreply.github.com>
2021-03-20 14:35:15 -0500

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,13 @@
+package lab4;
+
+public abstract class Food {
+	static int calories; // go here or in food class?
+	int temperature;
+	
+	public Food() {
+		System.out.println("Here comes food!");
+	}
+	
+	// abstract method implemented by child classes
+	abstract String eat();
+}
